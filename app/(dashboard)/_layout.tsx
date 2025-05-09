73ab2be878c03408cb6e6bc355004fc4f93ca02e
Aleksandar <aleksandar@e-vlasotince.info>
2025-05-09 02:29:32 +0200

    Tab bar icons and Tab navigation

--- app/(dashboard)/_layout.tsx
+++ app/(dashboard)/_layout.tsx
@@ -1,6 +1,7 @@
 import { Tabs } from "expo-router";
 import { useColorScheme } from "react-native";
 import { Colors } from "../../constants/colors";
+import { Ionicons } from "@expo/vector-icons";
 
 const DashboardLayout = () => {
   const colorSheme = useColorScheme();
@@ -12,16 +13,54 @@ const DashboardLayout = () => {
         headerShown: false,
         tabBarStyle: {
           backgroundColor: theme.navBackground,
+          // backgroundColor: "red",
           padding: 10,
-          height: 90,
+          height: 80,
         },
         tabBarActiveTintColor: theme.iconColorFocused,
         tabBarInactiveTintColor: theme.iconColor,
+        tabBarLabelStyle: { fontSize: 13, fontWeight: "bold" },
       }}
     >
-      <Tabs.Screen name="profile" options={{ title: "Profile" }} />
-      <Tabs.Screen name="books" options={{ title: "Books" }} />
-      <Tabs.Screen name="create" options={{ title: "Create" }} />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "person" : "person-outline"}
+              size={23}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="books"
+        options={{
+          title: "Books",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "book" : "book-outline"}
+              size={23}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="create"
+        options={{
+          title: "Create",
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons
+              name={focused ? "create" : "create-outline"}
+              size={23}
+              color={color}
+            />
+          ),
+        }}
+      />
     </Tabs>
   );
 };
